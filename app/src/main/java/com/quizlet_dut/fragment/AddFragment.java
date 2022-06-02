96afeb1e6bbacfee7768217fb7e9b2cef2e8209e
= <=phuthanhdo104@gmail.com>
2022-06-02 23:45:35 +0700

    Thanh creat new Category Success

--- app/src/main/java/com/quizlet_dut/fragment/AddFragment.java
+++ app/src/main/java/com/quizlet_dut/fragment/AddFragment.java
@@ -1,21 +1,121 @@
 package com.quizlet_dut.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.quizlet_dut.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddFragment extends Fragment {
 
+    FirebaseFirestore firestore;
+
+    EditText txtNumberOfTest,txtCategory;
+    Button btnAddCategory;
+    String id_category = "";
+    static int count = 0;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
+
+        firestore = FirebaseFirestore.getInstance();
+
+        txtNumberOfTest = view.findViewById(R.id.txt_NoOfTest);
+        txtCategory = view.findViewById(R.id.txt_category);
+
+        count = GetCountFromCategories();
+
+        btnAddCategory = view.findViewById(R.id.btn_addCategory);
+        btnAddCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                postDataToFirestore(txtCategory.getText().toString(), Integer.parseInt(txtNumberOfTest.getText().toString()));
+            }
+        });
+        return view;
     }
+
+    // đẩy dữ liệu lên FireStore
+    private void postDataToFirestore(String category, int no_of_quest){
+        Map<String, Object> data = new HashMap<>();
+        data.put("NAME",category);
+        data.put("NO_OF_TESTS",no_of_quest);
+        firestore.collection("QUIZ") //add category vào QUIZ
+                .add(data)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+
+                        firestore.collection("QUIZ").document(documentReference.getId()).update("CAT_ID",documentReference.getId());
+                        id_category = String.valueOf(documentReference.getId());
+                        Log.d("Debug", "DocumentSnapshot added with ID: " + id_category);
+                        UpdateNewCategoryAndCountToCategories(id_category);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Debug", "Error adding document", e);
+                    }
+                });
+    }
+
+    //ép một Object trở thành kiểu int
+    public static int objectToInt(Object obj)
+    {
+        int x = Integer.parseInt(obj.toString());
+        return x;
+    }
+
+    //lấy Field COUNT trong Categories rồi gán cho biến count
+    public int GetCountFromCategories(){
+        DocumentReference docRef = firestore.collection("QUIZ").document("Categories");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                DocumentSnapshot document = task.getResult();
+                if (task.isSuccessful()) {
+                    if (document.exists()) {
+                        count = objectToInt(document.getData().get("COUNT"));
+                        Log.d("Debug", "DocumentSnapshot Count of Categories: " + document.getData().get("COUNT"));
+                    } else {
+                        Log.d("Debug", "No such document");
+                    }
+                } else {
+                    Log.d("Debug", "get failed with ", task.getException());
+                }
+            }
+        });
+        return count;
+    }
+    
+    //update lại CAT_ID của category mới và tăng COUNT lên
+    public void UpdateNewCategoryAndCountToCategories(String id_Cat){
+        count++;
+        String CAT_ID = "CAT"+String.valueOf(count)+"_ID";
+        Log.d("Debug", "DocumentSnapshot CAT_ID: "+CAT_ID);
+        Log.d("Debug", "DocumentSnapshot id_cat: "+id_Cat);
+        firestore.collection("QUIZ").document("Categories").update("COUNT",count);
+        firestore.collection("QUIZ").document("Categories").update(CAT_ID,id_Cat);
+    }
+
 }
