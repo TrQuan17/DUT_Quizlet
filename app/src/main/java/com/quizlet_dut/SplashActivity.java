bb3d333f4e6c713ddd136285fb39f85177efca9d
thien <tranphuocthien2602@gmail.com>
2022-06-26 20:47:58 +0700

    fix

--- app/src/main/java/com/quizlet_dut/SplashActivity.java
+++ app/src/main/java/com/quizlet_dut/SplashActivity.java
@@ -51,7 +51,7 @@ public class SplashActivity extends AppCompatActivity {
                     DbQuery.loadData(new MyCompeleteListenner() {
                         @Override
                         public void onSuccess() {
-                            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                             startActivity(intent);
                             SplashActivity.this.finish();
                         }
